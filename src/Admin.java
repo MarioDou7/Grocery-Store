f462165ebb1c911d19527dfc59a59d8467d7a9c4
Mario_Dou7 <mariomamdouhreda@gmail.com>
2022-12-21 16:35:24 +0200

    adding functions to admin class

--- src/Admin.java
+++ src/Admin.java
@@ -12,21 +12,42 @@ public class Admin {
         return Instance;
     }
 
-    public void Check_Order_History(ShopOwner owner)
+    public void Check_Order_History(String owner)
     {
+        DatabaseHelper db = new DatabaseHelper();
+        db.ConnectDB();
         //owner_id = select ID from user where Username=owner
+        int owner_id = db.Select_ID_Username(owner);
+        if(owner_id == -1)
+            {
+                db.Close_Connection();
+                System.out.println("This owner doesn't exist");
+                return;
+            }
+        //product_ids = select p_id from products where owner_id =owner_id
+        int[] product_ids = db.Select_ProdID_ownerid(owner_id);
+        if(product_ids[0] == 00)
+        {
+            db.Close_Connection();
+            System.out.println("This owner doesn't exist");
+            return;
+        }
 
-        //product_ids = select p_id where owner_id =owner_id
-
-        // history = select * from transaction where p_id = products_ids
-
-        //return history
+        // select * from transaction where p_id = products_ids
+        db.filter_Transaction_ProdID(product_ids);
+        
+        db.Close_Connection();
     }
 
     public void View_Transcation()
     {
         DatabaseHelper db = new DatabaseHelper();
         db.ConnectDB();
-        
+
+        db.View_all_Table("Transactions");
+
+        db.Close_Connection();
     }
+
+    
 }
--- src/DatabaseHelper.java
+++ src/DatabaseHelper.java
@@ -25,7 +25,100 @@ public class DatabaseHelper {
     
     public void View_all_Table(String Table)
     {
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT * from ?");
+            stmt.setString(0, Table);
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                System.out.print(rs.toString());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
         
     }
     
+    public int Select_ID_Username(String Shoponwer)
+    {
+        int OwnerID = -1;
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT ID from users where Username=?");
+            stmt.setString(0, Shoponwer);
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next())
+                System.out.print(rs.toString());
+
+            OwnerID = rs.getInt(0);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return OwnerID;
+
+    }
+    
+    public int[] Select_ProdID_ownerid(int owner_id)
+    {
+        int[] product_ids ;
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT P_Id from Products where Owner_Id=?");
+            stmt.setInt(0, owner_id);
+
+            ResultSet rs = stmt.executeQuery();
+            
+            rs.last();                  // Place the record pointer onto the last row
+            int counter = rs.getRow(); // Get the row number (there's your count)
+            rs.first();   
+            
+            product_ids = new int[counter]; //specify the array size with length of rows returned
+            counter = 0;
+
+            while (rs.next())
+                System.out.print(rs.toString());
+                product_ids[counter] = rs.getInt("P_Id");
+
+            return product_ids;
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return new int[]{00};
+
+    }
+    
+    public void filter_Transaction_ProdID(int[] products_ids)
+    {
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT * from Transactions where P_Id=?");
+            for (int i = 0; i < products_ids.length; i++) {
+                
+                stmt.setInt(0, products_ids[i]);
+                ResultSet rs = stmt.executeQuery();
+
+                while (rs.next()){
+                    System.out.print(rs.toString());
+                }
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+
+    public void Close_Connection()
+    {
+        try {
+            con.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
 }
