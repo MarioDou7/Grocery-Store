f340b9a592797fb586b349480a27e5e4742dcb67
Mario_Dou7 <mariomamdouhreda@gmail.com>
2022-12-21 21:12:14 +0200

    DatabaseHelper done with functions about shop owners

---
+++ src/ShopOwner.java
@@ -0,0 +1,21 @@
+public class ShopOwner {
+    private String ShopName;
+    private Boolean is_Accepted;
+
+    public ShopOwner(){}
+    
+    public String getShopName() {
+        return ShopName;
+    }
+    public void setShopName(String shopName) {
+        ShopName = shopName;
+    }
+    public Boolean getIs_Accepted() {
+        return is_Accepted;
+    }
+    public void setIs_Accepted(Boolean is_Accepted) {
+        this.is_Accepted = is_Accepted;
+    }
+    
+    
+}
