fc5cc546c28b111ecea3968fcf2c042010244bb6
Mario_Dou7 <mariomamdouhreda@gmail.com>
2022-12-22 23:38:45 +0200

    Making Mange Products Proxy Desgin Pattern

--- Grocery2/src/main/java/model/ShopOwner.java
+++ Grocery2/src/main/java/model/ShopOwner.java
@@ -1,10 +1,16 @@
 package model;
 
 public class ShopOwner {
+    private int ID;
     private String ShopName;
     private Boolean is_Accepted;
 
-    public ShopOwner(){}
+    private MangeProducts manger ;
+
+
+    public ShopOwner(){
+        manger = new ProductsMangerProxy(is_Accepted);
+    }
 
     public String getShopName() {
         return ShopName;
@@ -19,5 +25,38 @@ public class ShopOwner {
         this.is_Accepted = is_Accepted;
     }
 
+    public void Add_Product(String ProductName,float Price,int Stock,String P_image,int category_id)
+    {
+        try {
+            manger.Add_product(ProductName,ID,Price,Stock,P_image,category_id);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
+    public void Edit_Product(String ProductName,float Price,int Stock,String Path_image,int category_id)
+    {
+        try {
+            manger.Edit_product(ProductName,Price,Stock,Path_image,category_id);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void Remove_Product(int P_ID)
+    {
+        try {
+            manger.Remove_Product(P_ID);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void Report_Best_Seller()
+    {
+
+    }
 }
