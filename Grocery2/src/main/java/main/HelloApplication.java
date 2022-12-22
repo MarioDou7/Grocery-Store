879430dc372bea454365541f7ca8f414f0600d75
MichaelLoka <62102221+MichaelLoka@users.noreply.github.com>
2022-12-22 15:33:31 +0200

    Add files via upload

---
+++ Grocery2/src/main/java/main/HelloApplication.java
@@ -0,0 +1,25 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        URL url = new File("D:\\ASU FCIS 2023\\Year 4\\Semester 1\\3. Software Design Patterns\\Project\\GUI\\Grocery2\\src\\main\\resources\\views\\market.fxml").toURI().toURL();
+        FXMLLoader fxmlLoader = new FXMLLoader(url);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ Grocery2/src/main/java/main/MyListener.java
@@ -0,0 +1,7 @@
+package main;
+
+import model.Item;
+
+public interface MyListener {
+    public void OnCLickListener(Item item);
+}
